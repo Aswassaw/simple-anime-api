65a114336015384e87a862c708d7c4e148f3ea6f
andry_pebrianto <andrypeb227@gmail.com>
2021-06-25 04:08:08 +0700

    Membuat model anime

---
+++ src/v1/model/anime.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const { db } = require('../config');
+
+let anime = db.define('anime', {
+    id: { type: Sequelize.INTEGER, primaryKey: true },
+    title: Sequelize.STRING,
+    synopsis: Sequelize.STRING,
+    type: Sequelize.STRING,
+    episodes: Sequelize.INTEGER,
+    premiered: Sequelize.STRING,
+    studios: Sequelize.STRING,
+    genres: Sequelize.STRING,
+    score: Sequelize.FLOAT,
+    source: Sequelize.STRING,
+    cover: Sequelize.STRING,
+}, {
+    freezeTableName: true,
+    timestamps: false,
+});
+
+module.exports = anime;
